468aefcc7162d4146ea9514f373f56869d8ba38e
Pelumiade <fecoyifemi@gmail.com>
2022-07-26 02:18:40 -0700

    task one

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,18 +1,14 @@
-#ifndef  MAIN_H
-#define  MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar (char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#include <stdlib.h>
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
-#endif	/*  MAIN_H */
+
+#endif /*MAIN_H*/
