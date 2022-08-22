cb04d0325c701ca04527159c940cae3bfb8a4ae7
Pelumiade <fecoyifemi@gmail.com>
2022-08-22 13:53:54 -0700

    task three

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,30 +39,6 @@ void check98(ssize_t check, char *file, int fd_from, int fd_to)
 	}
 }
 
-
-/**
- * check98 - checks that file_from exists and can be read
- * @check: checks if true of false
- * @file: file_from name
- * @fd_from: file descriptor of file_from, or -1
- * @fd_to: file descriptor of file_to, or -1
- *
- * Return: void
- */
-void check98(ssize_t check, char *file, int fd_from, int fd_to)
-{
-	if (check == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-		if (fd_from != -1)
-			close(fd_from);
-		if (fd_to != -1)
-			close(fd_to);
-		exit(98);
-	}
-}
-
-
 /**
  * check99 - checks that file_to was created and/or can be written to
  * @check: checks if true of false
@@ -84,7 +60,6 @@ void check99(ssize_t check, char *file, int fd_from, int fd_to)
 		exit(99);
 	}
 }
-
 
 /**
  * check100 - checks that file descriptors were closed properly
@@ -136,3 +111,4 @@ int main(int argc, char *argv[])
 	check100(close_to, fd_to);
 	check100(close_from, fd_from);
 	return (0);
+}
